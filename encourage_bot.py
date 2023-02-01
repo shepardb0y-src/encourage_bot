1c7f9c4efa7fb329f34e1e442679f06da6ea5284
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2023-01-31 21:15:20 -0500

    base bot

--- encourage_bot.py
+++ encourage_bot.py
@@ -1,1 +1,20 @@
 # first commit
+# This example requires the 'message_content' intent.
+
+import discord
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(
+    'MTA3MDAzNTAyNDUyMDQyOTYyOA.Gn76G3.vB9MAGQCrjh4aFoqxXk2JCUymxkrs3e6xflZmw')
